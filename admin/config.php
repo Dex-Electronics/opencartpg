ca82a5573844c65b406b498d21ad412b00c28d4e
Alf <dex.electronics@gmail.com>
2016-10-29 23:27:16 +0300

    first commit

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencartpg.dx/admin/');
+define('HTTP_CATALOG', 'http://opencartpg.dx/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencartpg.dx/admin/');
+define('HTTPS_CATALOG', 'http://opencartpg.dx/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/opencartpg/admin/');
+define('DIR_SYSTEM', '/var/www/opencartpg/system/');
+define('DIR_DATABASE', '/var/www/opencartpg/system/database/');
+define('DIR_LANGUAGE', '/var/www/opencartpg/admin/language/');
+define('DIR_TEMPLATE', '/var/www/opencartpg/admin/view/template/');
+define('DIR_CONFIG', '/var/www/opencartpg/system/config/');
+define('DIR_IMAGE', '/var/www/opencartpg/image/');
+define('DIR_CACHE', '/var/www/opencartpg/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/opencartpg/download/');
+define('DIR_LOGS', '/var/www/opencartpg/system/logs/');
+define('DIR_CATALOG', '/var/www/opencartpg/catalog/');
+
+// DB
+define('DB_DRIVER', 'postgre');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'usopencartpg');
+define('DB_PASSWORD', 'azsx12og');
+define('DB_DATABASE', 'opencartpg');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ system/library/db.php
@@ -0,0 +1,33 @@
+<?php
+class DB {
+	private $driver;
+	public $name;
+	
+	public function __construct($driver, $hostname, $username, $password, $database) {
+		if (file_exists(DIR_DATABASE . $driver . '.php')) {
+			$this->name = $driver;
+			require_once(DIR_DATABASE . $driver . '.php');
+		} else {
+			exit('Error: Could not load database file ' . $driver . '!');
+		}
+				
+		$this->driver = new $driver($hostname, $username, $password, $database);
+	}
+		
+  	public function query($sql) {
+		return $this->driver->query($sql);
+  	}
+	
+	public function escape($value) {
+		return $this->driver->escape($value);
+	}
+	
+  	public function countAffected() {
+		return $this->driver->countAffected();
+  	}
+
+  	public function getLastId() {
+		return $this->driver->getLastId();
+  	}	
+}
+?>
